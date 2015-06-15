70ac0095e84322f05f54867e9467d5fde2aef1c8
Matija Komorcec <matija_komorcec@yahoo.com>
2015-06-15 22:32:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
-﻿<?php
-include("includes/header.php");
-echo'
+<!DOCTYPE html>
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Matija Komorcec - Web Developer, photographer and an optimist!</title>
+        <link rel="stylesheet" href="CSS/reset.css" />
+        <link rel="stylesheet" href="CSS/style.css" />
+        <link href="http://fonts.googleapis.com/css?family=Clicker+Script|Oldenburg|Comfortaa|Baumans|Prosto+One" rel="stylesheet" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="JS/app.js"></script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCT-1IqrcfKimr3XkJNUMrPu8tnrbbQQqk"></script>
+        <script src="js/gmaps.js"></script>
+        <link rel="stylesheet" href="CSS/mediaqueries.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div id="container">
+            <div id="siteHeader">
+                <div id="nav" class="clear">
+                    <div id="info">
+
+                        <h1><img src="img/logo2.svg" class="logo" /><span class="blue">M</span>atija<span class="blue">K</span>omorcec</h1>
+                    </div>
+                    <div id="links" >
+                        <a href="#siteBody">Home</a>
+                        <a href="#skills">My Skills</a>
+                        <a href="#work">My Work</a>
+                        <a href="#contact">Contact</a>
+                    </div>             
+                </div>               
+            </div>
         <div id="hero">
             <h1>Hello! My name is <span class="name">Matija Komorčec.</span></h1>
-            <h2>And Im a web developer, photographer and an <span class="name">optimist.</span></h2>
+            <h2>And I'm a web developer, photographer and an <span class="name">optimist.</span></h2>
 			<h3>Get to know me</h3>
 			<img class="arrow" src="img/arrow.png" />
         </div>
@@ -77,6 +104,14 @@ echo'
 
         </div>
     </div>
-';
-include("includes/footer.php");
-?>
+<div id="siteFooter">
+            <h4>Handmade by Matija Komorcec &copy; 2015.</h4>
+            <div>Beard Icon made by 
+                <a href="http://www.freepik.com" title="Freepik">Freepik</a> from 
+                <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed under 
+                <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
